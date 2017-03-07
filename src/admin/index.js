e1df8b65bf1ddbf926675a4e1c1cd8b41cc49920
jovanidash21 <jwardash21@gmail.com>
2017-03-07 22:45:49 +0800

    Add React Router Redux Config

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from "react-redux";
-import App from './components/App';
-import store from "./store/index";
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux';
+import store from './store/index';
+import routes from './routes';
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 render(
     <Provider store={store}>
-        <App />
+        <Router history={history} routes={routes} />
     </Provider>
-    , document.getElementById('admin-root'));
+    , document.getElementById('admin-root')
+);
--- src/admin/reducers/index.js
+++ src/admin/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux'
 import namesReducer from './names';
 import activeNameReducer from './activeName';
 
 const reducers = combineReducers({
+    routing: routerReducer,
     names: namesReducer,
     activeName: activeNameReducer
 });
