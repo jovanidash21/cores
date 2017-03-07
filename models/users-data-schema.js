1f0ed5ec5a8b274b6a9995733fd5cf8e3aa88fa0
jovanidash21 <jwardash21@gmail.com>
2017-03-07 15:37:44 +0800

    Add Users Data Schema

---
+++ models/users-data-schema.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+var timestamps = require('mongoose-timestamp');
+
+mongoose.Promise = Promise;
+
+var usersDataSchema = new Schema
+(
+    {
+        firstName: {type:String, default: ''},
+        lastName: {type:String, default: ''},
+        email: {type:String, default: ''},
+        profileImage: {
+            type:String,
+            default: ''
+        },
+        role: {
+            type:String,
+            enum: ['administrator', 'subscriber', 'viewer'],
+            default: 'viewer'
+        },
+        birthDate: {type: Date, default: ''},
+        gender: {
+            type:String,
+            enum: ['male', 'female'],
+            default: ''
+        },
+        school: {type:String, default: ''},
+        studentNumber: {type:String, default: ''},
+        course: {type:String, default: ''},
+        socialLinks: [{
+            website: {type: String, default: ''},
+            facebook: {type: String, default: ''},
+            twitter: {type: String, default: ''},
+            instagram: {type: String, default: ''},
+            googleplus: {type: String, default: ''},
+            linkedin: {type: String, default: ''},
+            youtube: {type: String, default: ''},
+            github: {type: String, default: ''},
+            codepen: {type: String, default: ''}
+        }]
+    },
+    {
+        collection: 'usersData'
+    }
+);
+
+usersDataSchema.plugin(passportLocalMongoose);
+usersDataSchema.plugin(timestamps);
+
+module.exports = mongoose.model('usersData', usersDataSchema);
