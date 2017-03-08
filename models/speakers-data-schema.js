66b95c6ea3a05c19a812efa69f98248393387943
jovanidash21 <jwardash21@gmail.com>
2017-03-08 23:24:56 +0800

    Add Speakers Data Schema

---
+++ models/speakers-data-schema.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var Schema = mongoose.Schema;
+var timestamps = require('mongoose-timestamp');
+
+mongoose.Promise = Promise;
+
+var speakersDataSchema = new Schema
+(
+    {
+        firstName: {type:String, default: ''},
+        lastName: {type:String, default: ''},
+        position: {type:String, default: ''},
+        school: {type:String, default: ''},
+        office: {type:String, default: ''},
+        socialLinks: [{
+            website: {type: String, default: ''},
+            facebook: {type: String, default: ''},
+            twitter: {type: String, default: ''},
+            instagram: {type: String, default: ''},
+            googleplus: {type: String, default: ''},
+            linkedin: {type: String, default: ''},
+            youtube: {type: String, default: ''},
+            github: {type: String, default: ''},
+            codepen: {type: String, default: ''}
+        }]
+    },
+    {
+        collection: 'speakersData'
+    }
+);
+
+speakersDataSchema.plugin(timestamps);
+
+module.exports = mongoose.model('speakersData',speakersDataSchema);
