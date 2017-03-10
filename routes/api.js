93187f4f479452f511b2ceab36b7d597584e0d24
jovanidash21 <jwardash21@gmail.com>
2017-03-11 00:16:37 +0800

    Add Users GET And POST API

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usersData = require('../models/users-data-schema');
 
 router.get('/user', function(req, res, next) {
     if (req.user === undefined) {
@@ -7,6 +8,31 @@ router.get('/user', function(req, res, next) {
     }
     else {
         res.json([req.user]);
+    }
+});
+
+router.get('/users', function(req, res, next) {
+    if (req.user === undefined) {
+        res.json({});
+    }
+    else {
+        usersData.find()
+            .then(function (usersData) {
+                res.json([usersData]);
+            });
+    }
+});
+
+router.post('/users', function(req, res, next) {
+    if (req.user === undefined) {
+        res.redirect('/');
+    }
+    else {
+        usersData.register(new req.body.userData, req.body.password, function(err) {
+            if(err) {
+                res.end(err);
+            }
+        });
     }
 });
 
