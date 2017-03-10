0b1997881eba3baca88278d13b15f2029bf66892
jovanidash21 <jwardash21@gmail.com>
2017-03-10 19:54:20 +0800

    Update Admin New User Form Body

--- src/admin/containers/NewUserForm/Body.js
+++ src/admin/containers/NewUserForm/Body.js
@@ -31,22 +31,65 @@ const Body  = () => {
                     <div className="col-md-9">
                         <FormControl id="lastName" type="text" placeholder="" />
                     </div>
-                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="gender">
+                    <ControlLabel bsClass="col-md-3 control-label">
                         Gender
                     </ControlLabel>
                     <div className="col-md-9">
                         <Select2
-                            defaultValue={'male'}
-                            data={['male', 'female']}
+                            defaultValue={'Male'}
+                            data={['Male', 'Female']}
                         />
                     </div>
-                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="role">
+                    <ControlLabel bsClass="col-md-3 control-label">
+                        School
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <Select2
+                            defaultValue={'Polytechnic University of the Philippines (PUP) -Manila'}
+                            data={[
+                                'Polytechnic University of the Philippines (PUP) -Manila',
+                                'Arellano University (AU)',
+                                'Centro Escolar University (CEU)',
+                                'Eulogio "Amang" Rodriguez Institute of Science and Technology (EARIST)',
+                                'Far Eastern University (FEU)',
+                                'Jose Rizal University (JRU)',
+                                'National University (NU)',
+                                'Pamantasan ng Lungsod ng Maynila (PLM)',
+                                'Technological Institute of the Philippines (TIP)',
+                                'Technological University of the Philippines (TUP)',
+                                'University of the East (UE)',
+                                'University of Santo Tomas (UST)',
+                                'Other'
+                            ]}
+                        />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="studentNumber">
+                        Student Number
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <FormControl id="studentNumber" type="text" placeholder="" />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label">
+                        Course
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <Select2
+                            defaultValue={'BS Computer Engineering (BSCpE)'}
+                            data={[
+                                'BS Computer Engineering (BSCpE)',
+                                'BS Information Technology (BSIT)',
+                                'BS Computer Science (BSCS)',
+                                'Other'
+                            ]}
+                        />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label">
                         Role
                     </ControlLabel>
                     <div className="col-md-9">
                         <Select2
-                            defaultValue={'registrant'}
-                            data={['registrant', 'administrator']}
+                            defaultValue={'Registrant'}
+                            data={['Registrant', 'Administrator']}
                         />
                     </div>
                 </FormGroup>
