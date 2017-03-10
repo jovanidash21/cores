05ef77cbe613b841aed76fe8ddb7e36519130052
jovanidash21 <jwardash21@gmail.com>
2017-03-10 11:09:16 +0800

    Update Admin New User Form

---
+++ src/admin/containers/NewUserForm/Body.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+const Body  = () => {
+    return(
+        <div className="section">
+            <div className="section-body">
+                <FormGroup>
+                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="username">
+                        Username (required)
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <FormControl id="username" type="text" placeholder="" />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="email">
+                        Email (required)
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <FormControl id="email" type="email" placeholder="" />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="firstName">
+                        First Name
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <FormControl id="firstName" type="text" placeholder="" />
+                    </div>
+                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="lastName">
+                        Last Name
+                    </ControlLabel>
+                    <div className="col-md-9">
+                        <FormControl id="lastName" type="text" placeholder="" />
+                    </div>
+                </FormGroup>
+            </div>
+        </div>
+    )
+};
+
+export default Body;
--- src/admin/containers/NewUserForm/Index.js
+++ src/admin/containers/NewUserForm/Index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import Body from './Body/Index';
+import Body from './Body';
+import Footer from './Footer';
 
 const NewUserForm  = () => {
     return(
         <div className="row">
             <div className="col-md-12">
                 <div className="card">
-                    <Body />
+                    <div className="card-body">
+                        <form className="form form-horizontal">
+                            <Body />
+                            <Footer />
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
