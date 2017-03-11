c105d10572f476fef5f74f5fd33bce518c4eec3a
jovanidash21 <jwardash21@gmail.com>
2017-03-11 20:28:14 +0800

    Add React Redux Fetch Config

--- src/admin/reducers/index.js
+++ src/admin/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
+import { reducer as fetchReducer } from 'react-redux-fetch';
 import namesReducer from './names';
 import activeNameReducer from './activeName';
 
 const reducers = combineReducers({
     routing: routerReducer,
+    repository: fetchReducer,
     names: namesReducer,
     activeName: activeNameReducer
 });
--- src/admin/store/index.js
+++ src/admin/store/index.js
@@ -2,11 +2,12 @@ import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import { middleware as fetchMiddleware } from 'react-redux-fetch'
 import reducers from '../reducers/index';
 
 const store = createStore(
     reducers,
-    applyMiddleware(logger(), thunk, promise())
+    applyMiddleware(logger(), thunk, promise(), fetchMiddleware)
 );
 
 export default store;
