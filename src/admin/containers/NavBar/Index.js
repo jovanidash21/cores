8561a36fe38a49affbdeacc4cccdccb573792ddf
jovanidash21 <jwardash21@gmail.com>
2017-03-12 23:09:59 +0800

    Update Admin Nav Bar

--- src/admin/containers/NavBar/Index.js
+++ src/admin/containers/NavBar/Index.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-refetch';
+import LoadingAnimation from './LoadingAnimation';
+import Error from '../Error/Index';
 import Mobile from './Mobile';
 import Left from './Left';
 import Right from './Right';
 
-const NavBar = () => {
-    return(
-        <nav className="navbar navbar-default" id="navbar">
-            <div className="container-fluid">
-                <div className="navbar-collapse collapse in">
-                    <Mobile />
-                    <Left />
-                    <Right />
-                </div>
-            </div>
-        </nav>
-    )
-};
+class NavBar extends Component {
+    render() {
+        const { userDataFetch } = this.props;
+        if (userDataFetch.pending) {
+            return (
+                <LoadingAnimation />
+            )
+        }
+        else if (userDataFetch.rejected) {
+            return <Error error={userDataFetch.reason} />
+        }
+        else if (userDataFetch.fulfilled) {
+            const [ user ] = userDataFetch.value;
 
-export default NavBar;
+            return(
+                <nav className="navbar navbar-default" id="navbar">
+                    <div className="container-fluid">
+                        <div className="navbar-collapse collapse in">
+                            <Mobile />
+                            <Left />
+                            <Right
+                                user={user}
+                            />
+                        </div>
+                    </div>
+                </nav>
+            )
+        }
+    }
+}
+
+export default connect(() => {
+    const refreshUserData = {
+        userDataFetch: {
+            url: `/api/user`,
+            force: true,
+            refreshing: true
+        }
+    };
+
+    return {
+        userDataFetch: `/api/user`
+    }
+})(NavBar);
