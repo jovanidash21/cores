e9bc783353a9822746cdacf907e026b6b8047aa9
jovanidash21 <jwardash21@gmail.com>
2017-03-12 15:18:20 +0800

    Remove React Redux Fetch Config

--- src/admin/reducers/index.js
+++ src/admin/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
-import { reducer as fetchReducer } from 'react-redux-fetch';
 import namesReducer from './names';
 import activeNameReducer from './activeName';
 
 const reducers = combineReducers({
     routing: routerReducer,
-    repository: fetchReducer,
     names: namesReducer,
     activeName: activeNameReducer
 });
--- src/admin/store/index.js
+++ src/admin/store/index.js
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
-import { middleware as fetchMiddleware } from 'react-redux-fetch'
 import reducers from '../reducers/index';
 
 const store = createStore(
     reducers,
-    applyMiddleware(logger(), thunk, promise(), fetchMiddleware)
+    applyMiddleware(logger(), thunk, promise())
 );
 
 export default store;
