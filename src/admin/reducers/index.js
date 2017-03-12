ce9519e7bb074d00f7f841a387703bcb619140b7
jovanidash21 <jwardash21@gmail.com>
2017-03-12 16:28:47 +0800

    Delete Hello World Function

--- src/admin/reducers/index.js
+++ src/admin/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
-import namesReducer from './names';
-import activeNameReducer from './activeName';
 
 const reducers = combineReducers({
-    routing: routerReducer,
-    names: namesReducer,
-    activeName: activeNameReducer
+    routing: routerReducer
 });
 
 export default reducers;
