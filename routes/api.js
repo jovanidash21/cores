c376d1ca06f16a2d28461bc8dcf2a96b5b0b2be3
jovanidash21 <jwardash21@gmail.com>
2017-03-14 22:37:50 +0800

    Add Speakers GET And POST API

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var usersData = require('../models/users-data-schema');
 var seminarsData = require('../models/seminars-data-schema');
+var speakersData = require('../models/speakers-data-schema');
 
 router.get('/user', function(req, res, next) {
     if (req.user === undefined) {
@@ -79,6 +80,36 @@ router.post('/seminars', function(req, res, next) {
     else {
         var seminarData = new seminarsData(req.body);
         seminarData.save(function(err, results) {
+            if(err) {
+                res.end(err);
+            }
+            else {
+                res.json(results);
+            }
+        });
+    }
+});
+
+router.get('/speakers', function(req, res, next) {
+    if (req.user === undefined) {
+        res.json({});
+    }
+    else {
+        speakersData.find({}, function(err) {
+            if(err) {
+                res.end(err);
+            }
+        });
+    }
+});
+
+router.post('/speakers', function(req, res, next) {
+    if (req.user === undefined) {
+        res.redirect('/');
+    }
+    else {
+        var speakerData = new speakersData(req.body);
+        speakerData.save(function(err, results) {
             if(err) {
                 res.end(err);
             }
