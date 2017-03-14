5b776c85fc81a6840dc6c532ebcceb6e5b8d41b5
jovanidash21 <jwardash21@gmail.com>
2017-03-14 21:14:07 +0800

    Add Seminars GET And POST API

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var usersData = require('../models/users-data-schema');
+var seminarsData = require('../models/seminars-data-schema');
 
 router.get('/user', function(req, res, next) {
     if (req.user === undefined) {
@@ -53,6 +54,39 @@ router.post('/users', function(req, res, next) {
         usersData.register(new req.body.userData, req.body.password, function(err) {
             if(err) {
                 res.end(err);
+            }
+        });
+    }
+});
+
+router.get('/seminars', function(req, res, next) {
+    if (req.user === undefined) {
+        res.json({});
+    }
+    else {
+        seminarsData.find({}, function(err, results) {
+            if(err) {
+                res.end(err);
+            }
+            else {
+                res.json(results);
+            }
+        });
+    }
+});
+
+router.post('/users', function(req, res, next) {
+    if (req.user === undefined) {
+        res.redirect('/');
+    }
+    else {
+        var seminarData = new seminarsData(req.body);
+        seminarData.save(function(err, results) {
+            if(err) {
+                res.end(err);
+            }
+            else {
+                res.json(results);
             }
         });
     }
