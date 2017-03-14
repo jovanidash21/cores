832ad144333182a2fe5192ae404fa21c7679505e
jovanidash21 <jwardash21@gmail.com>
2017-03-14 22:58:22 +0800

    Add Admin Edit Seminar Form

---
+++ src/admin/components/EditSeminarForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EditSeminarFormContainer from '../containers/EditSeminarForm/Index';
+
+const EditSeminarForm  = () => {
+    return (
+        <EditSeminarFormContainer />
+    )
+};
+
+export default EditSeminarForm;
--- src/admin/routes.js
+++ src/admin/routes.js
@@ -4,6 +4,7 @@ import App from './containers/App';
 import Dashboard from './components/Dashboard';
 import AllSeminars from './components/AllSeminars';
 import NewSeminarForm from './components/NewSeminarForm';
+import EditSeminarForm from './components/EditSeminarForm'
 import AllUsers from './components/AllUsers';
 import NewUserForm from './components/NewUserForm';
 import UserProfile from './components/UserProfile';
@@ -11,8 +12,9 @@ import UserProfile from './components/UserProfile';
 export default (
     <Route path="/admin" component={App}>
         <IndexRoute component={Dashboard} />
-        <Route path="/admin/seminar/add" component={NewSeminarForm} />
         <Route path="/admin/seminars" component={AllSeminars} />
+        <Route path="/admin/seminar/add" component={NewSeminarForm} />
+        <Route path="/admin/seminar/:seminarID" component={EditSeminarForm} />
         <Route path="/admin/users" component={AllUsers} />
         <Route path="/admin/user/add" component={NewUserForm} />
         <Route path="/admin/user/:userID" component={UserProfile} />
