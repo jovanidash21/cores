9f65c4431e8a24bb90aebf1dabb8ab1c093f791a
jovanidash21 <jwardash21@gmail.com>
2017-03-15 08:23:23 +0800

    Update Admin New Seminar Form

--- src/admin/containers/NewSeminarForm/Body.js
+++ src/admin/containers/NewSeminarForm/Body.js
@@ -1,40 +1,74 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import {
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Button
+} from 'react-bootstrap';
 import Select2 from 'react-select2-wrapper';
 
-const Body  = () => {
-    return(
-        <div className="section">
-            <div className="section-body">
-                <FormGroup>
-                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="title">
-                        Title (required)
-                    </ControlLabel>
-                    <div className="col-md-9">
-                        <FormControl id="title" type="text" placeholder="" />
+class Body extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            genderValue: 'male',
+            schoolValue: 'pup-manila',
+            courseValue: 'bscpe',
+            roleValue: 'registrant'
+        };
+        this.handleGenderValueChange = this.handleGenderValueChange.bind(this);
+        this.handleSchoolValueChange = this.handleSchoolValueChange.bind(this);
+        this.handleCourseValueChange = this.handleCourseValueChange.bind(this);
+        this.handleRoleValueChange = this.handleRoleValueChange.bind(this);
+        this.handleAddNewUserSubmit = this.handleAddNewUserSubmit.bind(this);
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="section">
+                    <div className="section-body">
+                        <FormGroup>
+                            <ControlLabel bsClass="col-md-3 control-label" htmlFor="title">
+                                Title (required)
+                            </ControlLabel>
+                            <div className="col-md-9">
+                                <FormControl id="title" type="text" placeholder="" />
+                            </div>
+                            <ControlLabel bsClass="col-md-3 control-label">
+                                Speaker
+                            </ControlLabel>
+                            <div className="col-md-9">
+                                <Select2
+                                    defaultValue={'Speaker 1'}
+                                    data={[
+                                        'Speaker 1',
+                                        'Speaker 2'
+                                    ]}
+                                />
+                            </div>
+                            <ControlLabel bsClass="col-md-3 control-label" htmlFor="date">
+                                Schedule
+                            </ControlLabel>
+                            <div className="col-md-9">
+                                <FormControl id="date" type="text" placeholder="" />
+                            </div>
+                        </FormGroup>
                     </div>
-                    <ControlLabel bsClass="col-md-3 control-label">
-                        Speaker
-                    </ControlLabel>
-                    <div className="col-md-9">
-                        <Select2
-                            defaultValue={'Speaker 1'}
-                            data={[
-                                'Speaker 1',
-                                'Speaker 2'
-                            ]}
-                        />
+                </div>
+                <div className="form-footer">
+                    <div className="form-group">
+                        <div className="col-md-9 col-md-offset-3">
+                            <Button bsStyle="primary">
+                                Add New Seminar
+                            </Button>
+                        </div>
                     </div>
-                    <ControlLabel bsClass="col-md-3 control-label" htmlFor="date">
-                        Date
-                    </ControlLabel>
-                    <div className="col-md-9">
-                        <FormControl id="date" type="text" placeholder="" />
-                    </div>
-                </FormGroup>
+                </div>
             </div>
-        </div>
-    )
-};
+        )
+    }
+}
 
 export default Body;
--- src/admin/containers/NewSeminarForm/Index.js
+++ src/admin/containers/NewSeminarForm/Index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-refetch';
 import CardHeader from './CardHeader';
 import Body from './Body';
-import Footer from './Footer';
 
 class NewSeminarForm extends React.Component {
     render() {
@@ -23,4 +23,23 @@ class NewSeminarForm extends React.Component {
     }
 }
 
-export default NewSeminarForm;
+export default connect(() => {
+    const refreshSeminarsData = {
+        seminarsDataFetch: {
+            url: `/api/seminars`,
+            force: true,
+            refreshing: true
+        }
+    };
+
+    return {
+        addNewSeminar: (newUser, password) => ({
+            addNewSeminarFetch: {
+                url: `/api/seminars`,
+                method: 'POST',
+                body: JSON.stringify(newSeminar),
+                then: () => (refreshSeminarsData)
+            }
+        })
+    }
+})(NewSeminarForm);
