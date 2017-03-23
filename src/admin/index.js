4d79e06dbb57ef3cdee44edd3a632c9a3f5e8fcd
jovanidash21 <jwardash21@gmail.com>
2017-03-23 18:58:40 +0800

    Revert To React

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from "react-redux";
 import { Router, browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux';
-import store from './store/index';
 import routes from './routes';
 
-const history = syncHistoryWithStore(browserHistory, store);
-
 render(
-    <Provider store={store}>
-        <Router history={history} routes={routes} />
-    </Provider>
+    <Router history={browserHistory} routes={routes} />
     , document.getElementById('admin-root')
 );
