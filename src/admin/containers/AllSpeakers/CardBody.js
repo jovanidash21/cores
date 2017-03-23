8a6a8014f15d9cacd97e164bbfcbbe2896244d2d
jovanidash21 <jwardash21@gmail.com>
2017-03-23 09:49:35 +0800

    Fix Admin All Speakers Datatable

--- src/admin/containers/AllSpeakers/CardBody.js
+++ src/admin/containers/AllSpeakers/CardBody.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
-import TableRow from './TableRow';
+import { Link } from 'react-router';
+import SpicyDatatable from 'spicy-datatable';
+import moment from 'moment-timezone';
+import 'spicy-datatable/src/sample-styles.css'
 
 class CardBody extends Component {
     render() {
         const { speakers } = this.props;
+        const speakersColumns = [
+            { key: 'firstName', label: 'First Name' },
+            { key: 'lastName', label: 'Last Name' },
+            { key: 'position', label: 'Position' },
+            { key: 'school', label: 'School' },
+            { key: 'office', label: 'Office' },
+            { key: 'createdAt', label: 'Date Created' },
+            { key: 'updatedAt', label: 'Last Update' }
+        ];
+        const speakersRows = [];
+
+        for (var i = 0; i < speakers.length; i++) {
+            speakersRows.push({
+                firstName:
+                    <Link to={'/admin/speaker/' + speakers[i]._id}>
+                        {speakers[i].firstName}
+                    </Link>,
+                lastName:
+                    <Link to={'/admin/speaker/' + speakers[i]._id}>
+                        {speakers[i].lastName}
+                    </Link>,
+                position: speakers[i].position,
+                school: speakers[i].school,
+                office: speakers[i].office,
+                createdAt: moment(speakers[i].createdAt)
+                    .tz("Asia/Manila")
+                    .format("MM/DD/YYYY hh:mm A"),
+                updatedAt: moment(speakers[i].updatedAt)
+                    .tz("Asia/Manila")
+                    .format("MM/DD/YYYY hh:mm A")
+            });
+        }
 
         return(
             <div className="card-body no-padding">
-                <table className="datatable table table-striped primary" cellSpacing="0" width="100%">
-                    <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Position</th>
-                        <th>School</th>
-                        <th>Office</th>
-                        <th>Date Created</th>
-                        <th>Last Update</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        speakers.map(speaker =>
-                            <TableRow speaker={speaker} />
-                        )
-                    }
-                    </tbody>
-                </table>
+                <SpicyDatatable
+                    tableKey="seminarsTable"
+                    columns={speakersColumns}
+                    rows={speakersRows}
+                />
             </div>
         )
     }
