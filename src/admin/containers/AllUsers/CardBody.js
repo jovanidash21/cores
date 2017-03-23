c392e1c8a196c8aca5f663ffce59a40d18fc3abb
jovanidash21 <jwardash21@gmail.com>
2017-03-23 09:30:57 +0800

    Fix Admin All Users Datatable

--- src/admin/containers/AllUsers/CardBody.js
+++ src/admin/containers/AllUsers/CardBody.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
-import TableRow from './TableRow';
+import { Link } from 'react-router';
+import SpicyDatatable from 'spicy-datatable';
+import moment from 'moment-timezone';
+import 'spicy-datatable/src/sample-styles.css'
 
 class CardBody extends Component {
     render() {
         const { users } = this.props;
+        const usersColumns = [
+            { key: 'username', label: 'Username' },
+            { key: 'firstName', label: 'First Name' },
+            { key: 'lastName', label: 'lastName' },
+            { key: 'email', label: 'email' },
+            { key: 'role', label: 'role' },
+            { key: 'createdAt', label: 'Date Created' },
+            { key: 'updatedAt', label: 'Last Update' }
+        ];
+        const usersRows = [];
+
+        for (var i = 0; i < users.length; i++) {
+            usersRows.push({
+                username:
+                    <Link to={'/admin/user/' + users[i]._id}>
+                        {users[i].username}
+                    </Link>,
+                firstName: users[i].firstName,
+                lastName: users[i].lastName,
+                email: users[i].email,
+                role: users[i].role,
+                createdAt: moment(users[i].createdAt)
+                    .tz("Asia/Manila")
+                    .format("MM/DD/YYYY hh:mm A"),
+                updatedAt: moment(users[i].updatedAt)
+                    .tz("Asia/Manila")
+                    .format("MM/DD/YYYY hh:mm A")
+            });
+        }
 
         return(
             <div className="card-body no-padding">
-                <table className="datatable table table-striped primary" cellSpacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th>Username</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Date Created</th>
-                            <th>Last Update</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users.map(user =>
-                                <TableRow user={user} />
-                            )
-                        }
-                    </tbody>
-                </table>
+                <SpicyDatatable
+                    tableKey="usersTable"
+                    columns={usersColumns}
+                    rows={usersRows}
+                />
             </div>
         )
     }
