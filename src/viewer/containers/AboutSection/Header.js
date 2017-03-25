b60ad6226c613d3b1acb0a5ac437861fec0bc29e
jovanidash21 <jwardash21@gmail.com>
2017-03-25 22:02:37 +0800

    Update Viewer About Section

--- src/viewer/containers/AboutSection/Header.js
+++ src/viewer/containers/AboutSection/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="style1">
-            <h2>Let's take a look on who we are</h2>
+            <h2>Let's Take a Look on Who We Are</h2>
         </header>
     )
 };
